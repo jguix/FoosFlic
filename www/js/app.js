aac7312e55a64924aafea42dceb71bc9d62cf98a
Juan G. Jordán <juangui@gmail.com>
2016-04-30 18:34:27 +0200

    created defaults file

--- www/js/app.js
+++ www/js/app.js
@@ -1,32 +1,32 @@
-// Ionic Starter App
+  // Ionic Starter App
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-// @See https://github.com/gsklee/ngStorage
-angular.module('ionicApp', ['ionic', 'ngStorage'])
+  // angular.module is a global place for creating, registering and retrieving Angular modules
+  // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+  // the 2nd parameter is an array of 'requires'
+  // @See https://github.com/gsklee/ngStorage
+  angular.module('ionicApp', ['ionic', 'ngStorage'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+  .run(function($ionicPlatform) {
+    $ionicPlatform.ready(function() {
+      if(window.cordova && window.cordova.plugins.Keyboard) {
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+    cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
 
-      // Don't remove this line unless you know what you are doing. It stops the viewport
-      // from snapping when text inputs are focused. Ionic handles this internally for
-      // a much nicer keyboard experience.
-      cordova.plugins.Keyboard.disableScroll(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
-})
+        // Don't remove this line unless you know what you are doing. It stops the viewport
+        // from snapping when text inputs are focused. Ionic handles this internally for
+        // a much nicer keyboard experience.
+        cordova.plugins.Keyboard.disableScroll(true);
+      }
+      if(window.StatusBar) {
+        StatusBar.styleDefault();
+      }
+    });
+  })
 
-.config(function($stateProvider, $urlRouterProvider) {
+  .config(function($stateProvider, $urlRouterProvider) {
 
-  $stateProvider
+    $stateProvider
     .state('tabs', {
       url: "/tab",
       abstract: true,
@@ -60,24 +60,32 @@ angular.module('ionicApp', ['ionic', 'ngStorage'])
       }
     })
 
-   $urlRouterProvider.otherwise("/tab/queue");
+    $urlRouterProvider.otherwise("/tab/queue");
 
-})
+  })
 
 
-.controller('AppController', function($scope, $sessionStorage) {
-  //$ionicSideMenuDelegate.toggleLeft();
-  // Hook session storage to the scope
-  $scope.$storage = $sessionStorage;
-})
+  .controller('AppController', function($scope, $sessionStorage, $http) {
+    //$ionicSideMenuDelegate.toggleLeft();
+    // Hook session storage to the scope
+    //$scope.$storage = $sessionStorage;
 
-.controller("HomeController", function($scope, $sessionStorage) {
-  // Hook session storage to the scope
-  $scope.$storage = $sessionStorage.$default({
-    scoreLocal: 0,
-    scoreVisitor: 0,
-    queue : [],
-    players : [
+    // Load default data
+    $http.get('json/defaults.json').success(function (data) {
+      //console.log("data = " + JSON.stringify(data));
+      $scope.$storage = $sessionStorage.$default(data);
+    });
+
+  })
+
+  .controller("HomeController", function($scope, $sessionStorage) {
+    // Hook session storage to the scope
+    $scope.$storage = $sessionStorage;
+    /*$scope.$storage = $sessionStorage.$default({
+      scoreLocal: 0,
+      scoreVisitor: 0,
+      queue : [],
+      players : [
       {
         id : 0,
         name : "Modesto"
@@ -146,46 +154,46 @@ angular.module('ionicApp', ['ionic', 'ngStorage'])
         id : 16,
         name : "Invitado 4"
       }
-    ]
-  });
+      ]
+    });*/
 })
 
-.controller("MenuController", function($scope, $sessionStorage, $ionicPopover) {
-  // Hook session storage to the scope
-  $scope.$storage = $sessionStorage;
+  .controller("MenuController", function($scope, $sessionStorage, $ionicPopover) {
+    // Hook session storage to the scope
+    $scope.$storage = $sessionStorage;
 
-  $ionicPopover.fromTemplateUrl('templates/popover.html', {
-    scope: $scope,
-  }).then(function(popover) {
-    $scope.popover = popover;
-  });
+    $ionicPopover.fromTemplateUrl('templates/popover.html', {
+      scope: $scope,
+    }).then(function(popover) {
+      $scope.popover = popover;
+    });
 
-  $scope.selectPlayer = function(id) {
-    //$storage.selectedPlayer = id;
-    console.log("Selected player: " + id);
-    $scope.$storage.queue.push($scope.$storage.players[id]);
-    $scope.popover.hide();
-  }
+    $scope.selectPlayer = function(id) {
+      //$storage.selectedPlayer = id;
+      console.log("Selected player: " + id);
+      $scope.$storage.queue.push($scope.$storage.players[id]);
+      $scope.popover.hide();
+    }
 
-})
+  })
 
-.directive("playersQueue", function() {
-  return {
-    restrict : "E",
-    templateUrl : "templates/players-queue.html"
-  }
-})
+  .directive("playersQueue", function() {
+    return {
+      restrict : "E",
+      templateUrl : "templates/players-queue.html"
+    }
+  })
 
-.directive("playersStats", function() {
-  return {
-    restrict : "E",
-    templateUrl : "templates/players-stats.html"
-  }
-})
+  .directive("playersStats", function() {
+    return {
+      restrict : "E",
+      templateUrl : "templates/players-stats.html"
+    }
+  })
 
-.directive("scoreboard", function() {
-  return {
-    restrict : "E",
-    templateUrl : "templates/scoreboard.html"
-  }
-})
+  .directive("scoreboard", function() {
+    return {
+      restrict : "E",
+      templateUrl : "templates/scoreboard.html"
+    }
+  })
